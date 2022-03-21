cfb12d280a62f71906041a523b0cb9f6b3865a32
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-03-21 21:23:23 +0000

    Remove something random idk

--- core/__main__.py
+++ core/__main__.py
@@ -1,5 +1,5 @@
 from .config import Config
-from EpikCord import Client, Intents
+from EpikCord import Client, Intents, MessageTextInput, MessageActionRow, Modal
 
 class RoboEpik(Client):
     def __init__(self, config: Config):
@@ -9,4 +9,11 @@ class RoboEpik(Client):
         )
 
 client = RoboEpik(Config())
+
+@client.command(
+    description = "A testing modal."
+)
+async def test_modal(interaction):
+    await interaction.send_modal(Modal(title = "Testing!", custom_id="test_modal", components = MessageActionRow([MessageTextInput(custom_id="test_input", style = 1, label = "Foo.",placeholder="Enter something...")])))
+
 client.login()
