077da97530bd47ab7a87b1108e55fc2b8dde3ab7
Jerry.py <Theofficialjerrypy@gmail.com>
2022-03-26 18:37:46 -0400

    Add some message create

--- bot/utils/bot.py
+++ bot/utils/bot.py
@@ -1,6 +1,8 @@
 from .config import Config
 from EpikCord import Client, Intents
+from re import compile
 
+PULL_HASH_REGEX = compile(r'(?:(?P<org>(?:[A-Za-z]|\d|-)+)/)?(?P<repo>(?:[A-Za-z]|\d|-)+)?(?:##)(?P<index>[0-9]+)')
 
 class RoboEpik(Client):
     def __init__(self, *args, **kwargs):
@@ -15,3 +17,15 @@ class RoboEpik(Client):
 
     async def on_ready(self):
         print(f"I am ready! My name is {self.user.name}")
+    
+    def make_link(index, org=None, repo=None):
+        org = org or "Pycord-Development"
+        repo = repo or "pycord"
+        return f"https://github.com/{org}/{repo}/pull/{index}"
+
+    async def message_create(self, message):
+        links = list(set([self.make_link(index, org, repo) for org, repo, index in PULL_HASH_REGEX.findall(message.content)]))[:15]
+        if len(links) > 2:
+            links = [f"<{link}>" for link in links]
+        if links:
+            await message.reply("\n".join(links))
