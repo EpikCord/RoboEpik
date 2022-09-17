2b3e83a77b9cf417bd169bfc19e1e92bc11fa5be
nav-github01001 <krishnanav309@gmail.com>
2022-09-17 20:28:41 +0800

    fix(launcher): Fix not working in linux

--- launcher.py
+++ launcher.py
@@ -1,8 +1,16 @@
 from os import system
+from sys import platform
+
 
 print("Installing dependencies...")
 system("pip install -r requirements.txt --force-reinstall")
-system("cls")
-print("Done!")
-print("Starting up...")
-system("py -m bot")
+if platform.startswith('linux'):
+    system("clear")
+    print("Done!")
+    print("Starting up...")
+    system("python3 -m bot")
+else:
+    system("cls")
+    print("Done!")
+    print("Starting up...")
+    system("py -m bot")
